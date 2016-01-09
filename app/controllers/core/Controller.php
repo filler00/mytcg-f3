f3ed7a3a8ffc03b0044aae21e29d3215523d10de
Tatiana Bernardo <tatiana.mobe@gmail.com>
2016-01-09 23:05:20 +0000

    Implemented SQL Session

--- app/controllers/core/Controller.php
+++ app/controllers/core/Controller.php
@@ -8,31 +8,31 @@ use \DB\Jig;
 use \Session;
 
 class Controller {
-	
+
 	protected $f3;
 	protected $db;
 	protected $jig;
 	protected $auth;
-	
+
 	function __construct() {
 		$f3 = Base::instance();
-		
+
 		$this->db = new SQL(
 			'mysql:host=' . $f3->get('db_server') . ';port=' . $f3->get('db_port') . ';dbname=' . $f3->get('db_database'),
 			$f3->get('db_user'),
 			$f3->get('db_password')
 		);
 		$this->jig = new Jig('storage/jig/');
-		
+
 		$user = new SQL\Mapper($this->db, 'members');
 		$this->auth = new \Auth($user, array('id'=>'name', 'pw'=>'password'));
 		$this->f3 = $f3;
-		
+
 	}
-	
+
 	function beforeRoute() {
-		new Session();
-		
+		new \DB\SQL\Session($this->db);
+
 		// uncomment to make the user object available on each page when the user is signed in
 		/*
 		if ( $this->f3->exists('SESSION.userID')  ) {
@@ -41,9 +41,9 @@ class Controller {
 		}
 		*/
 	}
-	
+
 	function afterRoute() {
 		$this->f3->clear('SESSION.flash');
 	}
-	
+
 }
--- app/controllers/core/mytcg/Controller.php
+++ app/controllers/core/mytcg/Controller.php
@@ -10,38 +10,38 @@ use \Session;
 use Models\Core\Releases;
 
 class Controller {
-	
+
 	protected $f3;
 	protected $db;
 	protected $jig;
 	protected $auth;
 	protected $releases;
-	
+
 	function __construct() {
 		$f3 = Base::instance();
-		
+
 		$this->db = new SQL(
 			'mysql:host=' . $f3->get('db_server') . ';port=' . $f3->get('db_port') . ';dbname=' . $f3->get('db_database'),
 			$f3->get('db_user'),
 			$f3->get('db_password')
 		);
 		$this->jig = new Jig('storage/jig/');
-		
+
 		$user = new SQL\Mapper($this->db, 'members');
 		$this->auth = new \Auth($user, array('id'=>'name', 'pw'=>'password'));
 		$this->f3 = $f3;
-		
+
 		$this->releases = new Releases($this->db);
 		$this->f3->set('version', $this->releases->version);
 	}
-	
+
 	function beforeRoute() {
-		new Session();
-		
+		new \DB\SQL\Session($this->db);
+
 		if ( $this->f3->get('PARAMS.controller') != 'login' && !$this->f3->exists('SESSION.adminID') )
 			$this->f3->reroute('/mytcg/login');
 	}
-	
+
 	function afterRoute() {
 		$this->f3->clear('SESSION.flash');
 	}
