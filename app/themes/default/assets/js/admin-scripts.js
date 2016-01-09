9c5d86967ea60c6a64601851e495dbbceede361b
Tatiana Bernardo <tatiana.mobe@gmail.com>
2016-01-09 23:31:13 +0000

    Dashboard functionality

--- app/themes/default/assets/js/admin-scripts.js
+++ app/themes/default/assets/js/admin-scripts.js
@@ -6,16 +6,63 @@ $('.trigger-alert').on('click', function(e, verified){
     if ( !verified ) {
         e.preventDefault();
         var btn = $(this)[0];
-        
+
         $('#alert-modal').modal('show');
         $('#alert-modal .modal-title').text(btn.dataset.alertTitle);
         $('#alert-modal .modal-body').text(btn.dataset.alertMsg);
-        
+
         $('#alert-modal .alert-continue').on('click', function(){
             $(btn).trigger('click',true);
         });
     }
 });
+
+/********** NOTES **************************/
+
+	var note = $('#admin-notes');
+
+	var saveTimer;
+
+	note.on('input',function(e){
+		clearTimeout(saveTimer);
+    $('.loading').show("fast");
+		saveTimer = setTimeout(ajaxSaveNote, 2000);
+	}).trigger('input');
+
+	function ajaxSaveNote(){
+$.post( '', { 'note' : note.val() })
+  .done(function() {
+    $('.loading').hide();
+  });
+	}
+
+  /********** UPCOMING DECKS **************************/
+
+  // toggle upcoming/released deck fields
+  function statusToggle(form) {
+      if ( form.find('[name=status]').val() == "Upcoming" ) {
+          form.find('[name=filename]').closest('.form-group').hide();
+          form.find('[name=count]').closest('.row').hide();
+          form.find('[name=worth]').closest('.row').hide();
+          form.find('[name=masters]').closest('.form-group').hide();
+          form.find('[name=puzzle]').closest('.checkbox').hide();
+          form.find('[name=masterable]').closest('.checkbox').hide();
+      }
+      else {
+          form.find('div').show();
+      }
+  }
+  // edit cards form modal
+  $('#release-decks-modal').on('show.bs.modal', function (e) {
+  	$( "#release-decks-modal .modal-body" ).load('mytcg/cards/edit' + e.relatedTarget.dataset.deckStatus + '/' + e.relatedTarget.dataset.deckId, function(){
+  	    // hide/unhide fields
+  	    var form = $('#edit-cards-form');
+  	    statusToggle(form);
+  	    form.find('[name=status]').on('change', function(){
+              statusToggle(form);
+          });
+  	});
+  });
 
 /********** AFFILIATES **************************/
 
@@ -72,23 +119,23 @@ var gameList = new List('games', { valueNames: [ 'gamename' ] });
 /*****
  * edit games form modal
  *****/
- 
+
 $('#edit-game-modal').on('show.bs.modal', function (e) {
     $('#edit-game-modal .modal-header .nav-tabs [role=presentation]').removeClass('active');
     $('#edit-game-modal .modal-header .nav-tabs [role=presentation]:first-child').addClass('active');
 	$( "#edit-game-modal .modal-body" ).load('games/edit/' + e.relatedTarget.dataset.gameId, function(){
-	    
+
 	    // initialize tooltips
         $('[data-toggle="tooltip"]').tooltip({container: 'body'});
-        
+
         // disable event bubbling on current-round radio select buttons
         $(document).on('click', 'input[type=radio][name=current-round]', function(e){ e.stopPropagation(); });
-        
+
         // delete game round button- event handler
         $(document).on('click', 'button[name=delete-game-round]', function(){
             $(this).closest('.panel').remove();
         });
-        
+
         // new game round button - event handler
         $('button[name=new-game-round]').on('click', function(){
             if ( $('#game-rounds .panel').length > 0 ) {
@@ -98,16 +145,16 @@ $('#edit-game-modal').on('show.bs.modal', function (e) {
             }
             var newRound = parseInt(lastRound) + 1;
             var newRoundDiv = $('#new-game-round-panel > .panel').clone();
-            
+
             newRoundDiv.find('.panel-heading').prop('id','round' + newRound + '-header');
             newRoundDiv.find('.panel-heading').attr('href','#round' + newRound + '-collapse');
             newRoundDiv.find('.panel-heading').attr('aria-controls','round' + newRound + '-collapse');
             newRoundDiv.find('.panel-title .title-text').text('Round #' + newRound);
             newRoundDiv.find('.panel-collapse').prop('id', 'round' + newRound + '-collapse');
             newRoundDiv.find('.panel-collapse').prop('aria-labelledby', 'round' + newRound + '-header');
-            newRoundDiv.find('.panel-body .form-group').each(function(){ 
+            newRoundDiv.find('.panel-body .form-group').each(function(){
                 var field = $(this).find('label').text();
-                $(this).find('label').prop('for', 'rounds[' + newRound + '][' + field + ']'); 
+                $(this).find('label').prop('for', 'rounds[' + newRound + '][' + field + ']');
                 $(this).find('input').prop('name', 'rounds[' + newRound + '][' + field + ']');
             });
 
@@ -121,7 +168,7 @@ $('#edit-game-modal').on('show.bs.modal', function (e) {
 
 	    $('#edit-game-form [name="schedule-day"]').on('change', function(){
             var form = $(this).closest('form');
-            
+
             if ( $(this).val() === 'null' ) {
                 form.find('[name="schedule-frequency"]').prop('disabled',true);
             }
@@ -129,7 +176,7 @@ $('#edit-game-modal').on('show.bs.modal', function (e) {
                 form.find('[name="schedule-frequency"]').prop('disabled',false);
             }
         });
-	    
+
 	});
 });
 
@@ -142,10 +189,10 @@ if ( $('#new-game-form [name="schedule-day"]').val() === 'null' ) {
     $('#new-game-form [name="schedule-frequency"]').prop('disabled',true);
     $('#new-game-form [name="start-date"]').prop('disabled',true);
 }
-    
+
 $('#new-game-form [name="schedule-day"]').on('change', function(){
     var form = $(this).closest('form');
-    
+
     if ( $(this).val() === 'null' ) {
         form.find('[name="schedule-frequency"]').prop('disabled',true);
         form.find('[name="start-date"]').prop('disabled',true);
